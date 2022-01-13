a677688c80b0b3ebc86fd311adabfb1758919a86
TokmurzinIlia <ilia.tokmurzin@gmail.com>
2022-01-13 12:20:53 +0300

    Suit + @SelectPackages

--- src/test/java/byTextTest/SuiteTests.java
+++ src/test/java/byTextTest/SuiteTests.java
@@ -5,11 +5,13 @@ import byTextTest.testMethod.СheckElementCatalogSectionByTextTests;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
 
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
 
 @RunWith(JUnitPlatform.class)
-@SelectClasses({CheckElementComputersAndNetworksSectionsTests.class, СheckElementCatalogSectionByTextTests.class})
+//@SelectClasses({CheckElementComputersAndNetworksSectionsTests.class,СheckElementCatalogSectionByTextTests.class})
+@SelectPackages({"byTextTest.testMethod"})
 
 public class SuiteTests {
 }
