37262cbc0bf1d659e32283a7f6a95aadcd73932e
TokmurzinIlia <ilia.tokmurzin@gmail.com>
2022-01-17 12:25:59 +0300

    redact final

--- src/main/java/utils/chromeDriwer/MobileDriverChrome.java
+++ src/main/java/utils/chromeDriwer/MobileDriverChrome.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 public class MobileDriverChrome {
 
-    public static WebDriver driver;
+    private static WebDriver driver;
 
     private static void initializeChromeDriver() {
         WebDriverManager.chromedriver().setup();
